cb0c8cb55285b810cf4a7fd718af48351c255f1b
Masudi_Hamidu <masudihmfaume@gmail.com>
2022-09-14 10:13:47 +0300

    Kilometer to miles conversion

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import {useState} from 'react';
+import './App.css';
+import { type } from '@testing-library/user-event/dist/type';
+
+
+function Converter({ kilometre }){
+
+  function convert(kilometre){
+    return (kilometre/ 1.609);
+  }
+
+  return<>{convert(kilometre)}</>
+}
+
+const person = prompt(" Enter your name");
+if (person == null || person == "") {
+  alert( "You cancelled the prompt.");
+} else {
+  alert( "Hello! " + person + ", welcome to the kilometres to miles converter");
+} 
+
+function App() {
+  const [kilometre, setKilometre] = useState();
+  function handlechange(j){
+    setKilometre(j.target.value);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='box'>
+          <h4>Kilometre to Miles Converter</h4>
+       <input type="text" placeholder='Enter Kilometre here' value={kilometre} onChange ={handlechange} />
+          <p>{kilometre} is kilometre {<Converter kilometre={ kilometre }/>} is miles </p>
+          </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+export { Converter }
